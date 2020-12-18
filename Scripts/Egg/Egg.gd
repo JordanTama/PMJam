924e0104c23ae6ac4d1f774895b23a888bde899e
Dale Grant <dale.t.grant@student.uts.edu.au>
2020-12-18 14:22:13 +1100

    Egg destroyed on player collision

--- Scripts/Egg/Egg.gd
+++ Scripts/Egg/Egg.gd
@@ -1,12 +1,5 @@
 extends RigidBody2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
@@ -14,3 +7,8 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_Egg_body_entered(body):
+	if body is Player:
+		queue_free()
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,3 +1,4 @@
+class_name Player
 extends KinematicBody2D
 
 export var acceleration = 500
